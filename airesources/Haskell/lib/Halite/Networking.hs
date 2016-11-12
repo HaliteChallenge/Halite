2f16d1f2203b8da997dc840b8016cb504f6525c4
Jesse Paroz <jparoz@users.noreply.github.com>
2016-11-13 01:21:06 +1000

    Implement rest of starter pack (untested!)

--- airesources/Haskell/lib/Halite/Networking.hs
+++ airesources/Haskell/lib/Halite/Networking.hs
@@ -2,7 +2,7 @@ module Halite.Networking
   ( getInit
   , sendInit
   , getFrame
-  , sendMoves
+  , sendFrame
   ) where
 
 import Halite.Types
@@ -11,21 +11,41 @@ getInit :: IO (ID, Map)
 getInit = do
     userID <- read <$> getLine
     [w, h] <- map read . words <$> getLine
-    prod <- parseProductionMap (w, h) <$> getLine
+    prod <- map read . words <$> getLine
     mapc <- parseMapContents (w, h) prod <$> getLine
     return (userID, Map w h mapc)
 
 sendInit :: String -> IO ()
-sendInit = undefined
+sendInit = putStrLn
 
-getFrame :: IO Map
-getFrame = undefined
+getFrame :: Map -> IO Map
+getFrame (Map w h cont) =
+    Map w h . parseMapContents (w, h) (getProds cont) <$> getLine
 
-sendMoves :: [Move] -> IO ()
-sendMoves = undefined
+sendFrame :: [Move] -> IO ()
+sendFrame = putStrLn . unwords . map showMove
 
-parseProductionMap :: (Int, Int) -> String -> [[Int]]
-parseProductionMap = undefined
 
-parseMapContents :: (Int, Int) -> [[Int]] -> String -> [[Site]]
-parseMapContents = undefined
+parseMapContents :: (Int, Int) -> [Int] -> String -> [[Site]]
+parseMapContents (w, h) pds s = splitEvery w $ zipWith3 Site ons sts pds
+  where
+    (owners, sts) = splitMapContents (read <$> words s) (w * h)
+    ons = stretch owners
+    stretch (x:y:ys) = replicate x y ++ stretch ys
+    stretch [] = []
+
+splitMapContents :: [Int] -> Int -> ([Int], [Int])
+splitMapContents xs area = splitAt (length xs - area) xs
+
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery _ [] = []
+splitEvery n xs = first : splitEvery n rest
+  where
+    (first, rest) = splitAt n xs
+
+getProds :: [[Site]] -> [Int]
+getProds = map siteProduction . concat
+
+showMove :: Move -> String
+showMove (Move (Location x y) d) =
+    show x ++ " " ++ show y ++ " " ++ show (fromEnum d)
